704d512589fe6585e0c6b0f545189e6aea300e6b
Raymond <stunna617@yahoo.com>
2018-03-25 18:12:04 -0400

    init commit

---
+++ public/main.js
@@ -0,0 +1,66 @@
+var imgPath = ['me.png','burke.jpg','lee.jpg','mildred.jpg','roslindale.jpg','tobin.jpg']
+var pic = 0
+
+var carousel = ()=>{
+    if(pic >= imgPath.length-1){
+      pic = 0
+    }else{
+      pic = pic + 1;
+    }
+  $('#images').attr('src','img/' + imgPath[pic]);
+}
+$(()=>{
+  setInterval(carousel,3000)
+})
+$( document ).ready(function() {
+  $('.openList').on('click',function(){
+   $('.list').toggleClass('opened');
+  })
+});
+
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp':thumbUp
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+console.log('loaded');
